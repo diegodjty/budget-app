7127ce7c5ade2f89fb4f5b7fb4d0147b433b7b2d
Diego Taveras <diegotaveras2@outlook.com>
2020-02-16 20:45:39 -0500

    project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1>Weekly expenses</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
