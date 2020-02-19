413e4687a774bd26f6ac6fdc748592ee5c554db4
Diego Taveras <diegotaveras2@outlook.com>
2020-02-18 22:58:56 -0500

    Add initial functions to question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,37 @@
-import React,{Fragment} from 'react'
+import React,{Fragment, useState} from 'react'
 
 
 const Question = () => {
+
+    //Define state
+    const [quantity, handelQuantity] = useState(0);
+
+    //function that reads budget
+    const setBudget = e => {
+        handelQuantity(parseInt(e.target.value, 10))
+    }
+
+    // Submit to define budget
+    const addBudget = e => {
+        e.preventDefault();
+
+        // Validate
+
+        // If validation pass
+    }
+
     return (
         <Fragment>
             <h2>Set your Budget</h2>
 
-            <form action="">
+            <form
+                onSubmit={addBudget}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Set your budget"
+                    onChange={setBudget}
                 />
 
                 <input
